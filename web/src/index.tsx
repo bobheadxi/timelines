74ccadef4098f13a4d5d6f758dcaae31b944709b
Robert Lin <robertlin1@gmail.com>
2019-03-17 16:54:20 -0700

    web: init framework, styles
    closes #6

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import './styles/_all.scss';
+import Main from './components/Main/Main';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const UIkit: any = require('uikit');
+const Icons: any = require('uikit/dist/js/uikit-icons');
+
+// load the icon plugin
+UIkit.use(Icons);
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
