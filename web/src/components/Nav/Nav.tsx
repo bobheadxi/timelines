f4bcb560757201aa0428e3160b7582a570f4a3fb
Robert Lin <robertlin1@gmail.com>
2019-03-22 23:44:04 -0700

    web: fix 'about' exception for navbar

--- web/src/components/Nav/Nav.tsx
+++ web/src/components/Nav/Nav.tsx
@@ -20,7 +20,7 @@ class Nav extends Component<{
         </div>
 
         <div className="uk-navbar-right">
-          {!this.props.noTitles && location.pathname != "about"
+          {!this.props.noTitles && location.pathname != "/about"
             ? <div className="uk-navbar-item title title-m">
               <a href="/about" className="uk-link-heading">About</a>
             </div>
