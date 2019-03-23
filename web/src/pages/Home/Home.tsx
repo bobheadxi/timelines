7cdf680267f9c3796c0062b7ab5aeb62e46906e0
Robert Lin <robertlin1@gmail.com>
2019-03-23 00:36:19 -0700

    web: adjust styles

--- web/src/pages/Home/Home.tsx
+++ web/src/pages/Home/Home.tsx
@@ -12,30 +12,32 @@ class Home extends Component<{
     return (
       <div >
         <Nav noTitles={true} location={location} />
-        <div className="margin-sides-l">
+        <div>
           <header className="flex ai-center jc-center ">
             <img src={banner} alt="banner" height="100px" width="85%" />
           </header>
 
-          <div
-            className="uk-child-width-1-2@s uk-grid-match"
-            data-uk-scrollspy="target: > div; cls:uk-animation-fade; delay: 50"
-            data-uk-grid>
-            <div>
-              <div className="uk-card uk-card-hover uk-card-default">
-                <div className="uk-card-body">
-                  <h3 className="uk-card-title">About</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                  <a href="/about" className="uk-button uk-button-text">Read more</a>
+          <div className="margin-sides-xxl">
+            <div
+              className="uk-child-width-1-2@s uk-grid-match"
+              data-uk-scrollspy="target: > div; cls:uk-animation-fade; delay: 50"
+              data-uk-grid>
+              <div>
+                <div className="uk-card uk-card-hover uk-card-default">
+                  <div className="uk-card-body">
+                    <h3 className="uk-card-title">About</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <a href="/about" className="uk-button uk-button-text">Read more</a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div>
-              <div className="uk-card uk-card-hover uk-card-default">
-              <div className="uk-card-body">
-                <h3 className="uk-card-title">Demo</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                  <a href="/github.com/bobheadxi/calories" className="uk-button uk-button-text">See the demo</a>
+              <div>
+                <div className="uk-card uk-card-hover uk-card-default">
+                <div className="uk-card-body">
+                  <h3 className="uk-card-title">Demo</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <a href="/github.com/bobheadxi/calories" className="uk-button uk-button-text">See the demo</a>
+                  </div>
                 </div>
               </div>
             </div>
--- web/src/pages/Timeline/Timeline.tsx
+++ web/src/pages/Timeline/Timeline.tsx
@@ -18,28 +18,12 @@ class Timeline extends Component<{
     const { host, owner, name } = this.props.match.params;
 
     return (
-      <div >
+      <div>
         <Nav location={location} />
-
-        <div className="text-center title title-m pad-bot-m">
-          {`${host}/${owner}/${name}`}
-        </div>
-
         <div className="margin-sides-l">
-          <div className="uk-child-width-1-2@s uk-grid-match " data-uk-grid>
-            <div>
-              <div className="uk-card uk-card-hover uk-card-default uk-card-body">
-                  <h3 className="uk-card-title">Default</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-              </div>
-            </div>
-            <div>
-              <div className="uk-card uk-card-hover uk-card-default uk-card-body">
-                  <h3 className="uk-card-title">Primary</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-              </div>
-            </div>
-          </div>
+          <h1 className="uk-heading-line uk-text-center pad-bot-l margin-sides-l">
+            <span>{`${host}/${owner}/${name}`}</span>
+          </h1>
         </div>
       </div>
     );
