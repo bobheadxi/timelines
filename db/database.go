40e6c0965d673635745f25a76e958e03e9b0091a
Robert Lin <robertlin1@gmail.com>
2019-03-31 18:01:17 -0700

    db: move copyFromRows to database.go

--- db/database.go
+++ db/database.go
@@ -63,3 +63,27 @@ func (db *Database) Pool() *pgx.ConnPool { return db.pg }
 
 // Close disconnects from the database
 func (db *Database) Close() { db.pg.Close() }
+
+// copyFromRows returns a CopyFromSource interface over the provided rows slice
+// making it usable by *Conn.CopyFrom.
+func copyFromRows(vals [][]interface{}) pgx.CopyFromSource {
+	return &rows{rows: vals, idx: -1}
+}
+
+type rows struct {
+	rows [][]interface{}
+	idx  int
+}
+
+func (r *rows) Next() bool {
+	r.idx++
+	return r.idx < len(r.rows) && r.rows[r.idx] != nil
+}
+
+func (r *rows) Values() ([]interface{}, error) {
+	return r.rows[r.idx], nil
+}
+
+func (r *rows) Err() error {
+	return nil
+}
