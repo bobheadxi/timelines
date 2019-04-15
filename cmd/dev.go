07e25a5a565a6a3402fb2da6bbed587ca824d599
Robert Lin <robertlin1@gmail.com>
2019-04-15 00:03:21 -0700

    cmd: add more robust conn options

--- cmd/dev.go
+++ cmd/dev.go
@@ -8,6 +8,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 
+	"github.com/bobheadxi/timelines/config"
 	"github.com/bobheadxi/timelines/db"
 	"github.com/bobheadxi/timelines/dev"
 	"github.com/bobheadxi/timelines/host"
@@ -27,11 +28,13 @@ func newDevCommand() *cobra.Command {
 
 func newPGCommand() *cobra.Command {
 	var (
-		pg = &cobra.Command{
+		fromEnv bool
+		pg      = &cobra.Command{
 			Use:   "pg",
 			Short: "postgres database utilities",
 		}
 	)
+	pg.PersistentFlags().BoolVar(&fromEnv, "from-env", false, "use env settings instead of hardcoded dev settings")
 
 	var (
 		seed = &cobra.Command{
@@ -42,11 +45,22 @@ func newPGCommand() *cobra.Command {
 				if err != nil {
 					return err
 				}
-				var l = logger.Named("dev.pg.seed")
-				c, err := db.New(l, "integration_test", dev.DatabaseOptions)
+
+				var (
+					l = logger.Named("dev.pg.seed")
+					c *db.Database
+				)
+				if fromEnv {
+					l.Info("loading config from env")
+					c, err = db.New(l, "integration_test", config.NewDatabaseConfig())
+				} else {
+					l.Info("loading local development config")
+					c, err = db.New(l, "integration_test", dev.DatabaseOptions)
+				}
 				if err != nil {
 					return err
 				}
+
 				return c.Repos().NewRepository(context.Background(),
 					host.HostGitHub,
 					dev.GetTestInstallationID(),
@@ -61,11 +75,13 @@ func newPGCommand() *cobra.Command {
 
 func newRedisCommand() *cobra.Command {
 	var (
-		redis = &cobra.Command{
+		fromEnv bool
+		redis   = &cobra.Command{
 			Use:   "redis",
 			Short: "redis store utilities",
 		}
 	)
+	redis.PersistentFlags().BoolVar(&fromEnv, "from-env", false, "use env settings instead of hardcoded dev settings")
 
 	var (
 		reset = &cobra.Command{
@@ -76,11 +92,22 @@ func newRedisCommand() *cobra.Command {
 				if err != nil {
 					return err
 				}
-				var l = logger.Named("dev.redis.reset")
-				c, err := store.NewClient(l, "dev.redis.reset", dev.StoreOptions)
+
+				var (
+					l = logger.Named("dev.redis.reset")
+					c *store.Client
+				)
+				if fromEnv {
+					l.Info("loading config from env")
+					c, err = store.NewClient(l, "dev.redis.reset", config.NewStoreConfig())
+				} else {
+					l.Info("loading local development config")
+					c, err = store.NewClient(l, "dev.redis.reset", dev.StoreOptions)
+				}
 				if err != nil {
 					return err
 				}
+
 				c.Reset()
 				return nil
 			},
@@ -96,8 +123,18 @@ func newRedisCommand() *cobra.Command {
 				if err != nil {
 					return err
 				}
-				var l = logger.Named("dev.redis.seed")
-				c, err := store.NewClient(l, "dev.redis.seed", dev.StoreOptions)
+
+				var (
+					l = logger.Named("dev.redis.seed")
+					c *store.Client
+				)
+				if fromEnv {
+					l.Info("loading config from env")
+					c, err = store.NewClient(l, "dev.redis.seed", config.NewStoreConfig())
+				} else {
+					l.Info("loading local development config")
+					c, err = store.NewClient(l, "dev.redis.seed", dev.StoreOptions)
+				}
 				if err != nil {
 					return err
 				}
