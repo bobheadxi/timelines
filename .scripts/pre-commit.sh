129b9e08bce3137b2b4aca3b8b62c6a3e69fe9cf
Robert Lin <robertlin1@gmail.com>
2019-04-23 01:18:57 -0700

    chore: add much improved pre-commit

--- .scripts/pre-commit.sh
+++ .scripts/pre-commit.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -o pipefail
 
-# run linters
-make lint
+PASS=0
+
+# Golang checks
+STAGED_GO_FILES=$(git diff --cached --name-only | grep ".go$" || true)
+if [[ "$STAGED_GO_FILES" != "" ]]; then
+  echo ">> info: go files were staged, running go checks"
+  echo ">> check: go vet"
+  go vet ./...
+  if [[ $? == 1 ]]; then
+    PASS=1
+  fi
+  echo ">> check: gofmt and golint"
+  for FILE in $STAGED_GO_FILES
+  do
+    gofmt -d "$FILE"
+    if [[ $? == 1 ]]; then
+      PASS=1
+    fi
+    golint "-set_exit_status" "$FILE"
+    if [[ $? == 1 ]]; then
+      PASS=1
+    fi
+  done
+  echo ">> info: go checks ok"
+fi
+
+if [[ $PASS == 1 ]]; then
+  echo ">> error: some checks failed"
+  exit 1
+else
+  echo ">> success: everything looks good"
+  exit 0
+fi
