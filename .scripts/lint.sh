31d39fa0b1650f476cc1af5d87e93c90654dfb13
Robert Lin <robertlin1@gmail.com>
2019-04-22 23:32:29 -0700

    chore: add go vet to pre-commit lint.sh

--- .scripts/lint.sh
+++ .scripts/lint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+go vet ./...
 FILES=`find . -type f -name '*.go' -not -path "./vendor/*" -not -path "*/testdata/*"`
 diff -u <(echo -n) <(gofmt -d -s $FILES)
 diff -u <(echo -n) <(golint `go list ./... | grep -v /vendor/`)
